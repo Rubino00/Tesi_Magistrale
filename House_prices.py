19bb039932d2902782fa5b6f1024236f0ec9e4a8
Francesco_Rubino <frarubino00@gmail.com>
2025-04-16 20:54:04 +0200

    importato dataset e creato nuovo file.py per l'elaborazione del prezzo di una casa

---
+++ House_prices.py
@@ -0,0 +1,8 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+input_path = "C:\\Users\\fraru\OneDrive\Desktop\programmi\Clone_python\Tesi_magistrale\Datastes\House_prices\House_prices_dataset.csv"
+df = pd.read_csv (input_path)
+
