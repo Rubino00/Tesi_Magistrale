f20117a3ffb859349c5a4c88a36ac22943d80227
Francesco_Rubino <frarubino00@gmail.com>
2025-04-27 12:50:13 +0200

    definite alcune funzioni per la manipolazione dei dati vettoriali

--- Sitellite_elaborator.py
+++ Sitellite_elaborator.py
@@ -1,30 +1,51 @@
 
+
 import pandas as pd
 import numpy as np
 import seaborn as sbn
 import rasterio
 import geopandas as gpd
 import matplotlib.pyplot as plt
-file_path = "C:\\Users\\fraru\OneDrive\Desktop\programmi\Clone_python\Tesi_magistrale\Datastes\Sentinel_images\sentinel image.tif"
-raster = rasterio.open (file_path)
+from matplotlib.colors import ListedColormap
 
-# definiamo una funzione che ha lo scopo di fornire informazioni generali al raster
-def info_raster (raster):
-    print (f"numero bande: {raster.count}")
-    print (f"dimensiona immagine: {raster.width} x {raster.height}")
-    print (f"sistema di riferimento: {raster.crs}")
-    print (f"formato del dato: {raster.dtypes}")
-    print (f"attributo transform: {raster.transform}")
+file_path_raster = "C:\\Users\\fraru\OneDrive\Desktop\programmi\Clone_python\Tesi_magistrale\Datastes\Sentinel_images\sentinel image.tif"
+raster = rasterio.open (file_path_raster)
 
-def raster_to_data (raster_array:np.array , band_list:list):
+file_path_vectorial = "C:\\Users\\fraru\OneDrive\Desktop\Qgis\cartine shapefile e QGIS\cartina dell'Italia x comuni\Com2011_WGS84\Com2011_WGS84.shp"
+data_vector = gpd.read_file (file_path_vectorial)
+
+# definiamo una funzione che ha lo scopo di visualizzare il generico dizionario
+def print_dict (dict_gen: dict):
+    for key in dict_gen:
+        print (f"{key}: {dict_gen [key]}")
+
+
+# definiamo una funzione in grado di stampare informazioni di un dataframe
+def info_df (df):
+    print (df.info ())
+    print (df.isnull ().sum ())
+    print (df.head (30))
+
+# definiamo una funzione per convertire il raster in dataframe di pandas riportando le bande alle colonne
+def raster_to_data (raster_array: np.array , band_list: list):
     reshaped_array = raster_array.reshape (raster_array.shape [0] , -1).T
     df_raster = pd.DataFrame (reshaped_array , columns = band_list)
-    return reshaped_array
+    return df_raster
 
 
-band_list = ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8" , "SCL"]
-raster_array = raster.read ()
-reshaped_array = raster_to_data (raster_array , band_list)
+# definiamo una funzione che trasformi il dataframe in un array
+def data_to_raster (df: pd.DataFrame , row , column):
+    count = len (df.columns)
+    return df.to_numpy ().T.reshape ((count , row , column))
 
 
+# definiamo una funzione di normalizzazione, i valori delle bande si rappresenteranno con un valore
+# compreso tra 0 e 1
+def normalize_band (band):
+    max_val = band.max ()
+    normalized_band = band / max_val
+    return normalized_band
+
+# elenco colonne: 'OBJECTID', 'COD_REG', 'COD_PRO', 'COD_ISTAT', # 'PRO_COM', 'NOME', 'SHAPE_Leng',
+# 'SHAPE_Area', 'geometry'
 
